cfcd4c9ccb6b1e3510f01deb70fec46a6a3d78b3
xlaez <utibeabasiekong2@gmail.com>
2024-01-16 20:39:36 +0100

    update: return http exception rather than 200 for error messages

--- server/routes/user.py
+++ server/routes/user.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Body
+from fastapi import APIRouter, Body, HTTPException, status
 from fastapi.encoders import jsonable_encoder
 from server.utils.hash_password import (hash_password, compare_password)
 from server.utils.auth_tokens import (generate_access_token, generate_refresh_token)
@@ -34,12 +34,12 @@ async def login(user: UserLoginModel = Body(...)):
     present_user = await retrieve_user_email(jsonable_encoder(user)["email"])
 
     if len(present_user) ==0 :
-        return ErrorResponseModel("Error", 400, "User not found, try signing up")
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, ErrorResponseModel("Error", 400, "User not found, try signing up"))
     if(compare_password(jsonable_encoder(user)["password"], present_user["password"])):
         access_token =  generate_access_token(present_user["id"])
         refresh_token = generate_refresh_token(present_user["id"])
     else:
-        return ErrorResponseModel("Error",400, "login credentials incorrect")
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, ErrorResponseModel("Error", 400, "Login credentials incorrect"))
     return SuccessResponseModel({"access_token": access_token, "refresh_token": refresh_token}, "User logged in successfully")
 
 @router.get("/all", response_description="Users fetched")
@@ -47,14 +47,14 @@ async def get_users():
     users = await retrieve_users()
     if users:
         return SuccessResponseModel(users, "Users data fetched")
-    return ErrorResponseModel("Error", 404, "Database empty, there are no users yet")
+    raise HTTPException(status.HTTP_404_NOT_FOUND, ErrorResponseModel("Error", 404, "Database empty, there are no users yet"))
 
 @router.get("/{id}", response_description="User data retrieved")
 async def get_user_by_id(id):
     user = await retrieve_user(id)
     if user:
         return SuccessResponseModel(user, "User data fetched")
-    return ErrorResponseModel("Error", 404, "User does not exist")
+    raise HTTPException(status.HTTP_404_NOT_FOUND, ErrorResponseModel("Error", 404, "User does not exist"))
 
 @router.put("/{id}")
 async def update_user_data(id: str, req: UpdateUserModel = Body(...)):
@@ -63,11 +63,11 @@ async def update_user_data(id: str, req: UpdateUserModel = Body(...)):
 
     if updated_user:
         return SuccessResponseModel("User with ID: {} updated successfully".format(id), "Success")
-    return ErrorResponseModel("Error", 400, "Cannot update user data")
+    raise HTTPException(status.HTTP_400_BAD_REQUEST, ErrorResponseModel("Error", 400, "Cannot update user data"))
 
 @router.delete("/{id}")
 async def delete_user_data(id:str):
     deleted_user  = await delete_user(id)
     if deleted_user:
         return SuccessResponseModel("User with ID: {} deleted".format(id), "Successfully deleted user")
-    ErrorResponseModel("Error", 400, "Cannot delete user from database")
+    raise HTTPException(status.HTTP_400_BAD_REQUEST, ErrorResponseModel("Error", 400, "Cannot delete user from database"))
